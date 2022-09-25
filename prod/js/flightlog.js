e0d58ade9faf600ff3effd5dc548b5043fff7d1c
mikedbrown <mike.del.brown@gmail.com>
2022-09-25 07:54:07 -0600

    Initial stub for flight log

---
+++ prod/js/flightlog.js
@@ -0,0 +1,51 @@
+'use strict';
+// Globals
+const now = new Date()
+const date = new Intl.DateTimeFormat('en-US', {month: '2-digit', day: '2-digit', year: 'numeric'}).format(now)
+const wwGrn = '#20c997' // Bootstrap teal
+const wwYlw = '#ffc107' // Bootstrap yellow (warning)
+const wwOrg = '#fd7e14' // Bootstrap orange
+const wwRed = '#dc3545' // Bootstrap red (danger)
+
+// Set default initial page
+let currentDiv = 'Enter New Flight'
+document.getElementById('current-div').innerHTML = currentDiv
+
+window.onclick = function(event) {
+    if (!event.target.matches('.btn-menu')) {
+        const menu = document.getElementById('menu')
+        if (menu.classList.contains('show')) menu.classList.remove('show')
+    }
+}
+
+function menu() { document.getElementById('menu').classList.toggle('show') }
+
+function reload() {
+    history.scrollRestoration = 'manual'
+    location.reload()
+}
+
+function toggleDiv(newDiv) {
+    document.getElementById(currentDiv).style.display = 'none'
+    currentDiv = newDiv
+    document.getElementById(currentDiv).scrollTop = 0
+    document.getElementById('current-div').innerHTML = currentDiv
+    document.getElementById(currentDiv).style.display = 'block'
+}
+
+// Make CORS requests to external sites via proxy server
+function doCORSRequest(options, result) {
+    var cors_api_url = 'https://wasatchcloudbase.herokuapp.com/'
+    var ServerRequest = new XMLHttpRequest()
+    ServerRequest.open(options.method, cors_api_url + options.url)
+    ServerRequest.onload = ServerRequest.onerror = function() {
+        result(ServerRequest.responseText)
+        // Can add to result for debugging:  options.method + ' ' + options.url + '\n' + ServerRequest.status + ' ' + ServerRequest.statusText + '\n\n' +
+    }
+    ServerRequest.send(options.data);
+}
+
+// Set defaults for flight log entry
+(async () => {
+    document.getElementById('flightDate').innerText = date()
+})();
