53a16ad0128d2a23b3838ec92ef19f6de6e6f487
mikedbrown <mike.del.brown@gmail.com>
2022-10-07 12:21:08 -0600

    Flight log proto update

--- prod/js/flightlog.js
+++ prod/js/flightlog.js
@@ -12,6 +12,8 @@ let currentLocation = 'POTM North Side'
 document.getElementById('btn-location-menu').innerHTML = currentLocation 
 let currentGlider = 'Gin Explorer'
 document.getElementById('btn-glider-menu').innerHTML = currentGlider
+let currentBuddyList = null
+document.getElementById('btn-buddies-menu').innerHTML = currentBuddyList
 
 
 // Set flight log entry default date
@@ -20,25 +22,12 @@ let day = today.getDate(); // typeof = number
 let month = ('0' + (today.getMonth()+1)).slice(-2) // typeof = number
 let year = today.getFullYear(); // typeof = number
 let currentDate = `${year}-${month}-${day}`; // typeof = string;
-document.getElementById("flightDate").value = currentDate;
-
-window.onclick = function(event) {
-    if (!event.target.matches('.btn-menu')) {
-        const menu = document.getElementById('menu')
-        if (menu.classList.contains('show')) menu.classList.remove('show')
-    }
-/*    if (!event.target.matches('.btn-location-menu')) {
-        const LocMenu = document.getElementById('location-dropdown')
-        if (LocMenu.classList.contains('show')) LocMenu.classList.remove('show')
-    }
-*/
-}
+document.getElementById('flightDate').value = currentDate;
 
 function menu() { document.getElementById('menu').classList.toggle('show') }
-
 function locationMenu() { document.getElementById('location-dropdown').classList.toggle('show') }
-
 function gliderMenu() { document.getElementById('glider-dropdown').classList.toggle('show') }
+function buddyMenu() { document.getElementById('buddies-dropdown').classList.toggle('show') }
 
 function reload() {
     history.scrollRestoration = 'manual'
@@ -46,6 +35,7 @@ function reload() {
 }
 
 function toggleDiv(newDiv) {
+    document.getElementById('menu').classList.toggle('show')
     document.getElementById(currentDiv).style.display = 'none'
     currentDiv = newDiv
     document.getElementById(currentDiv).scrollTop = 0
@@ -65,6 +55,12 @@ function selectGlider(newGlider) {
     document.getElementById('btn-glider-menu').innerHTML = currentGlider
 }
 
+function selectBuddy(newBuddy) {
+    document.getElementById('buddies-dropdown').classList.toggle('show')
+    if (currentBuddyList) { currentBuddyList = currentBuddyList + ', ' + newBuddy }
+    else { currentBuddyList = newBuddy }
+    document.getElementById('btn-buddies-menu').innerHTML = currentBuddyList
+}
 
 // Make CORS requests to external sites via proxy server
 function doCORSRequest(options, result) {
